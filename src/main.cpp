99a643a038e9b0d2dd695a1b96f950b3674c8296
Anubhav Rajput <rajanubhav@gmail.com>
2026-03-17 09:00:21 +0530

    implement distance measurement using HC-SR04 sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,25 @@
 #include <Arduino.h>
-/**
- * @file main.cpp
- * @brief Embedded Distance Measurement using Ultrasonic Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
- *
- * @details
- * Measures distance using HC-SR04 ultrasonic sensor
- * and displays structured output via Serial Monitor.
- */
+#define trigPin 9
+#define echoPin 10
 
- // TODO 1:
- // Define TRIG pin (Use pin 9)
-
- // TODO 2:
- // Define ECHO pin (Use pin 10)
-
- // TODO 3:
- // Create variable to store duration
-
- // TODO 4:
- // Create variable to store calculated distance
-
-void setup() {
-
-    // TODO 5:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 6:
-    // Configure TRIG as OUTPUT
-
-    // TODO 7:
-    // Configure ECHO as INPUT
-
-    // TODO 8:
-    // Print system initialization message
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
-void loop() {
+void loop()
+{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance=duration*0.034/2;
+    Serial.println("Distance:");
+    Serial.println("cm");
+    delay(100);
 
-    // TODO 9:
-    // Set TRIG LOW for 2 microseconds
-
-    // TODO 10:
-    // Send 10 microsecond pulse on TRIG
-
-    // TODO 11:
-    // Measure pulse duration on ECHO using pulseIn()
-
-    // TODO 12:
-    // Calculate distance in cm
-
-    // TODO 13:
-    // Print calculated distance
-
-    // TODO 14:
-    // Add delay (500ms)
 }
